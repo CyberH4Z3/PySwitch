c6c0b5d9d500559ae16b5394632fb182ace33459
CyberH4Z3 <122421202+CyberH4Z3@users.noreply.github.com>
2025-04-07 01:27:02 -0400

    Update communication.py

--- content/communication.py
+++ content/communication.py
@@ -1,45 +1,43 @@
 ##############################################################################################################################################
-# 
-# Definition of communication wrappers. This is where the client specific (i.e. Kemper) implementations are linked to the framework.
-#
+# Definition of communication wrappers. This is where the client-specific (i.e., Kemper) implementations are linked to the framework.
 ##############################################################################################################################################
 
+# Import necessary components
 from pyswitch.clients.kemper import KemperBidirectionalProtocol
-
 from pyswitch.controller.midi import MidiRouting
 from pyswitch.hardware.devices.pa_midicaptain import PA_MIDICAPTAIN_USB_MIDI
 
-# MIDI Devices in use (optionally you can specify the in/out channels here, too)
+# Constants for configuration
+DEFAULT_IN_CHANNEL = None  # All channels
+DEFAULT_OUT_CHANNEL = 0
+DEFAULT_PROTOCOL_TIME_LEASE_SECONDS = 30
+
+# MIDI Devices in use
 _USB_MIDI = PA_MIDICAPTAIN_USB_MIDI(
-    in_channel = None,  # All
-    out_channel = 0
+    in_channel=DEFAULT_IN_CHANNEL,
+    out_channel=DEFAULT_OUT_CHANNEL
 )
 
 # Communication configuration
 Communication = {
-
     # Optional: Protocol to use. If not specified, the standard Client protocol is used which requests all
     # parameters in each update cycle. Use this to implement bidirectional communication.
     "protocol": KemperBidirectionalProtocol(
-        time_lease_seconds = 30               # When the controller is removed, the Profiler will stay in bidirectional
-                                              # mode for this amount of seconds. The communication is re-initiated every  
-                                              # half of this value. 
+        time_lease_seconds=DEFAULT_PROTOCOL_TIME_LEASE_SECONDS  # Time before re-initiating bidirectional communication
     ),
 
-    # MIDI setup. This defines all MIDI routings. You at least have to define routings from and to 
-    # the MidiController.PYSWITCH source/target or the application will not be able to communicate!
+    # MIDI setup: Defines all MIDI routings for communication
     "midi": {
         "routings": [
             # Application: Receive MIDI messages from USB
             MidiRouting(
-                source = _USB_MIDI,
-                target = MidiRouting.APPLICATION
+                source=_USB_MIDI,
+                target=MidiRouting.APPLICATION
             ),
-
             # Application: Send MIDI messages to USB
             MidiRouting(
-                source = MidiRouting.APPLICATION,
-                target = _USB_MIDI
+                source=MidiRouting.APPLICATION,
+                target=_USB_MIDI
             ),
         ]
     }
